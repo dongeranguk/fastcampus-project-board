a3063438d38b7c1d8e845f8d982f7518265cd561
dongeranguk <rooetym@naver.com>
2022-11-28 23:39:01 +0900

    #12 - AuditingFields 추출
    생성자, 생성일시, 수정자, 수정일시는 반복적으로
    엔티티 클래스에 들어가는 요소이고,
    도메인과 직접 연관이 없는 요소이므로
    추출이 가능하다.
    
    `@MappedSuperclass` 이용하여 상속 방식으로 추출함.
    
    그외 '@DateTimeFormat` 요소 추가 및 jpa 옵션(+updatable) 개선

--- src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
@@ -11,26 +11,19 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
-@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Table(indexes = {
         @Index(columnList = "content"),
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy")
 })
-public class ArticleComment {
+public class ArticleComment extends AuditingFields {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Setter @ManyToOne(optional = false) private Article article; // 게시글 (Article)
     @Setter @Column(nullable = false, length = 500) private String content; // 본문
-
-    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; // 생성일자
-    @CreatedBy @Column(nullable = false, length = 100) private String createdBy; // 생성자
-    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; // 수정일자
-    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy; // 수정자
-
     protected ArticleComment() {}
 
     private ArticleComment(Article article, String content) {
