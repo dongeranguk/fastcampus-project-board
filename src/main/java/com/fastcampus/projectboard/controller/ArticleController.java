65f0ddb54e0790edf3d7d0cd0b8bf5e8969c2c70
dongeranguk <rooetym@naver.com>
2023-02-21 23:04:33 +0900

    #36 - 리팩토링 : UserAccountDto 에 메타 정보를 넣지 않는 of() 팩토리 메소드 추가
    테스트를 용이하도록 하기 위해 추가함
    
    테스트 목적 외에도,
    repository 레이어에 영속되지 않는 객체는
    메타 정보가 없는 것이 당연하므로
    새로운 데이터를 만들 때 이 부분을 입력하지 않아도 되는 팩토리 메소드를 만들어주는 것이 의미가 있음

--- src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
+++ src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
@@ -83,8 +83,7 @@ public class ArticleController {
     public String postNewArticle(ArticleRequest articleRequest) {
         // TODO : 인증 정보를 넣어줘야 한다.
         articleService.saveArticle(articleRequest.toDto(UserAccountDto.of(
-                "du", "asdf1234", "du@mail.com", "Du", "memo", null, null, null, null
-        )));
+                "du", "asdf1234", "du@mail.com", "Du", "memo")));
 
         return "redirect:/articles";
     }
@@ -103,8 +102,7 @@ public class ArticleController {
     public String updateArticle(@PathVariable Long articleId, ArticleRequest articleRequest) {
         // TODO : 인증 정보를 넣어줘야 한다.
         articleService.updateArticle(articleId, articleRequest.toDto(UserAccountDto.of(
-                "du", "asdf1234", "du@mail.com", "Du", "memo", null, null, null, null
-        )));
+                "du", "asdf1234", "du@mail.com", "Du", "memo")));
 
         return "redirect:/articles/" + articleId;
     }
