0d7b24c5c12067dec4249237f4ca5c0fe44555d4
dongeranguk <rooetym@naver.com>
2023-06-09 00:04:38 +0900

    #86 - 컨트롤러 테스트 오류 해결
    컨트롤러에서는 `SecurityConfig` 클래스의 `userDetailsService` 메소드가 사용하는 빈이 등록 되어 있지 않으므로, 빈을 찾지 못하는 오류 발생
    
    이에 대응하기 위해 `SecurityConfig` 클래스의 룰을 따르면서, 테스트를 위한 `TestSecurityConfig` 클래스를 생성하여 이를 불러와 테스트를 진행하도록 하였다.
    
    `TestSecurityConfig` 클래스에서 컨트롤러에서 사용하지 않았던 빈을 mocking 하였으므로 테스트를 통과할 수 있다.

--- src/test/java/com/fastcampus/projectboard/controller/AuthControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/AuthControllerTest.java
@@ -1,22 +1,23 @@
 package com.fastcampus.projectboard.controller;
 
-import com.fastcampus.projectboard.config.SecurityConfig;
-import org.junit.jupiter.api.Disabled;
+import com.fastcampus.projectboard.config.TestSecurityConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestComponent;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @DisplayName("View 컨트롤러 - 인증")
-@Import(SecurityConfig.class)
-@WebMvcTest
+@Import(TestSecurityConfig.class)
+@WebMvcTest(AuthControllerTest.EmptyController.class)
 public class AuthControllerTest {
 
     private final MockMvc mvc;
@@ -33,4 +34,11 @@ public class AuthControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
+
+    /**
+     * 어떤 컨트롤러도 필요하지 않은 테스트임을 나타내기 위해 테스트용 빈 컴포넌트를 사용함.
+     */
+
+    @TestComponent
+    static class EmptyController {}
 }
